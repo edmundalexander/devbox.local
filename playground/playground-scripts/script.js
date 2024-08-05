8f866523ac1be16caf6b6fc62535d07b5a814a7a
Edmund Alexander <edmundalexandersemails@gmail.com>
2024-08-05 05:03:41 +0100

    Imgs, js

---
+++ playground/playground-scripts/script.js
@@ -0,0 +1,9 @@
+function add (num1, num2) {
+    var num1 = num1;
+    var num2 = num2;
+    const answer = num1 + num2;
+    if (answer > "a") {
+        console.log("Invalid");
+    } else {
+        console.log(answer);}
+    }
