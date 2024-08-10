595381cd9c42772438d1add5700706d6098be6cf
Edmund Alexander <edmundalexandersemails@gmail.com>
2024-08-10 02:48:39 +0100

    js & header changes

--- js/main-script.js
+++ js/main-script.js
@@ -1,8 +1,3 @@
-function pagesizer () {
-    let headerHeight = document.querySelector("header").clientHeight;
-    let footerHeight = document.querySelector("footer").clientHeight;
-    let outerHeights = headerHeight + footerHeight;
-    console.log(headerHeight);
-    console.log(footerHeight);
-    console.log(outerHeights);
+function imglightbox () {
+    console.log("Lightbox clicked!")
 }
